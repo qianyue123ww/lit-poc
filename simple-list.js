fd2387d372da4e05b04935f80c51771a52b08d92
dengwenwen <dengwenwen@baidu.com>
2022-10-14 04:21:37 +0800

    add translayer

--- simple-list.js
+++ simple-list.js
@@ -1,38 +1,51 @@
 import { html, css, LitElement } from "lit";
 import "./simple-card.js";
-import "./trans-layer.js";
+import { TransLayer } from "./trans-layer.js";
 import {until} from 'lit/directives/until.js';
 
 import data from './mockData';
-export class SimpleList extends LitElement {
+
+const TransLayerPage = TransLayer(LitElement);
+export class SimpleList extends TransLayerPage {
     static properties = {
         loaded: {}
     }
-    static styles = css`
-        .list-wrap {
-            width: 1000px;
-            margin: auto;
-        }
-    `
+    static styles = [
+        TransLayerPage.styles || [],
+        css`
+            .list-wrap {
+                width: 1000px;
+                margin: auto;
+            }
+        `
+    ]
     constructor() {
         super();
-        this.loaded = false;
-        console.log(this.loaded)
+        this.setTransLayer(false);
         setTimeout(() => {
-            this.loaded = true;
-            console.log('this loaded', this.loaded)
+            this.setTransLayer(true);
         }, 2000);
     }
 
     render() {
-        return html `
-            <div class="list-wrap">
-                ${data.map(v => {
-                    return html`<simple-card .data=${v}></simple-card>`
-                })}
-            </div>
-            <div ?hidden=${this.loaded}><trans-layer></trans-layer></div>
-        `
+        return this.renderEle(
+            html`
+                <div class="list-wrap">
+                    ${data.map(v => {
+                        return html`<simple-card .data=${v}></simple-card>`
+                    })}
+                </div>
+            `
+        );
+        // return html `
+        //     <div class="list-wrap">
+        //         ${data.map(v => {
+        //             return html`<simple-card .data=${v}></simple-card>`
+        //         })}
+        //     </div>
+        //     <div ?hidden=${this.loaded}><trans-layer></trans-layer></div>
+        // `
+
     }
 }
 
--- trans-layer.js
+++ trans-layer.js
@@ -1,67 +1,79 @@
 import { LitElement, css, html } from 'lit';
-
-export class TransLayer extends LitElement {
-	static styles = css`
-		:host {
-			position: fixed;
-			display: block;
-			opacity: .8;
-			left: 0;
-			right: 0;
-			bottom: 0;
-			top: 0;
-			z-index: 1000;
-			user-select: none;
-			background-color: #eee;
+import {classMap} from 'lit/directives/class-map.js';
+export const TransLayer = (superClass) => {
+	class TransLayerEle extends superClass {
+		static properties = {
+			transLayerLoaded: {}
 		}
-		.progress-bar {
-			position: fixed;
-			top: 0;
-			left: 0;
-			right: 0;
-			height: 3px;
-			z-index: 1001;
-		}
-		.bar {
-			// animation: animate .1s cubic-bezier(0.4,0.0,1,1) infinite;
-			animation: animate 1s infinite;
-			animation-timing-function: cubic-bezier(0.4,0.0,1,1);
-			animation-delay: .1s;
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 25%;
-			height: 100%;
-			background: #68e;
-			transform: scaleX(0);
-			transform-origin: 0 0;
-		}
-
-		@keyframes animate {
-			0% {
+		static styles = css`
+			.mask-layer {
+				position: fixed;
+				display: block;
+				opacity: .8;
+				left: 0;
+				right: 0;
+				bottom: 0;
+				top: 0;
+				z-index: 1000;
+				user-select: none;
+				background-color: #eee;
+			}
+			.progress-bar {
+				position: fixed;
+				top: 0;
+				left: 0;
+				right: 0;
+				height: 3px;
+				z-index: 1001;
+			}
+			.bar {
+				// animation: animate .1s cubic-bezier(0.4,0.0,1,1) infinite;
+				animation: animate 1s infinite;
+				animation-timing-function: cubic-bezier(0.4,0.0,1,1);
+				animation-delay: .1s;
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 25%;
+				height: 100%;
+				background: #68e;
 				transform: scaleX(0);
+				transform-origin: 0 0;
 			}
-			50% {
-				transform: scaleX(5);
+
+			@keyframes animate {
+				0% {
+					transform: scaleX(0);
+				}
+				50% {
+					transform: scaleX(5);
+				}
+				100% {
+					transform: scaleX(5) translateX(100%);
+				}
 			}
-			100% {
-				transform: scaleX(5) translateX(100%);
-			}
+		`
+		constructor() {
+			super();
+			this.transLayerLoaded = false;
 		}
-	`
-	static properties = {
-	};
 
-	constructor() {
-		super();
-	}
+		renderEle(content) {
+			return html`
+				${content}
+				<div ?hidden=${this.transLayerLoaded}>
+					<div class="progress-bar">
+						<div class="bar"></div>
+					</div>
+					<div class="mask-layer"></div>
+				</div>
+			`;
+		}
 
-	render() {
-		return html`
-		<div class="progress-bar">
-			<div class="bar"></div>
-		</div>
-    `
+		setTransLayer(flag) {
+			this.transLayerLoaded = flag;
+		}
 	}
+	return TransLayerEle;
 }
-customElements.define('trans-layer', TransLayer);
+
